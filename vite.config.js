8c5dbaabe80f4cb41a407695d507f5bd9d49bff7
DESKTOP-K2GASL6\Angel <carmenFGDev@gmail.com>
2024-03-04 20:28:23 +0100

    server

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,19 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   envPrefix: 'PUBLIC_',
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+    onwarn(warning, warn) {
+      if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+        return
+      }
+      warn(warning)
+    }}
+  },
+
   plugins: [
     react({
       babel: {
@@ -12,15 +24,16 @@ export default defineConfig({
     }),
     splitVendorChunkPlugin(),
   ],
+  
   resolve: {
     alias: {
-      assets: path.resolve(__dirname, 'src/assets'),
-      common: path.resolve(__dirname, 'src/common'),
+     /* assets: path.resolve(__dirname, 'src/assets'),
+      common: path.resolve(__dirname, 'src/common'),*/
       core: path.resolve(__dirname, 'src/core'),
-      layouts: path.resolve(__dirname, 'src/layouts'),
+     /* layouts: path.resolve(__dirname, 'src/layouts'),
       pods: path.resolve(__dirname, 'src/pods'),
       scenes: path.resolve(__dirname, 'src/scenes'),
-      'common-app': path.resolve(__dirname, 'src/common-app'),
+      'common-app': path.resolve(__dirname, 'src/common-app'),*/
     },
   },
 });
